2e06422ed0834aef380d3c6bd0f27f3bfee4b9ab
ryankwteo <ryankt2@illinois.edu>
2018-03-06 20:38:16 -0600

    Added a bunch of stuff I still can't comprehend right now.

--- src/main/java/MorseDecoder.java
+++ src/main/java/MorseDecoder.java
@@ -55,6 +55,14 @@ public class MorseDecoder {
         for (int binIndex = 0; binIndex < totalBinCount; binIndex++) {
             // Get the right number of samples from the inputFile
             // Sum all the samples together and store them in the returnBuffer
+            int frameRead = inputFile.readFrames(sampleBuffer, BIN_SIZE);
+            returnBuffer[binIndex] = 0;
+            for (int sampleCount = 0; sampleCount < sampleBuffer.length; sampleCount++) {
+                returnBuffer[binIndex] += Math.abs(sampleBuffer[sampleCount]);
+            }
+            if (frameRead < BIN_SIZE && binIndex != totalBinCount - 1) {
+                throw new RuntimeException("short read from Wav file");
+            }
         }
         return returnBuffer;
     }
@@ -64,6 +72,27 @@ public class MorseDecoder {
 
     /** Bin threshold for dots or dashes. Related to BIN_SIZE. You may need to modify this value. */
     private static final int DASH_BIN_COUNT = 8;
+
+    /**
+     * Something happens.
+     *
+     * @param powerCount continuous streak of power
+     * @return dot, dash, space, or empty
+     */
+    private static String powerCountToDash(final int powerCount) {
+        if (powerCount > 0) {
+            if (powerCount > DASH_BIN_COUNT) {
+                return "-";
+            } else {
+                return ".";
+            }
+        } else if (powerCount < 0) {
+            if (Math.abs(powerCount) > DASH_BIN_COUNT) {
+                return " ";
+            }
+        }
+        return "";
+    }
 
     /**
      * Convert power measurements to dots, dashes, and spaces.
@@ -86,8 +115,26 @@ public class MorseDecoder {
         // else if ispower and not waspower
         // else if issilence and wassilence
         // else if issilence and not wassilence
-
-        return "";
+        int sawPowerCount = 0;
+        String returnString = " ";
+        for (int binIndex = 0; binIndex < powerMeasurements.length; binIndex++) {
+            double powerMeasurement = powerMeasurements[binIndex];
+            if (powerMeasurement > POWER_THRESHOLD) {
+                if (sawPowerCount < 0) {
+                    returnString += powerCountToDash(sawPowerCount);
+                    sawPowerCount = 0;
+                }
+                sawPowerCount++;
+            } else {
+                if (sawPowerCount > 0) {
+                    returnString += powerCountToDash(sawPowerCount);
+                    sawPowerCount = 0;
+                }
+            }
+            sawPowerCount--;
+        }
+        returnString += powerCountToDash(sawPowerCount);
+        return returnString;
     }
 
     /**
